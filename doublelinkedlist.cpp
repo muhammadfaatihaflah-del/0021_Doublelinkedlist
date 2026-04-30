15ba007b350fcdd5c1dca841d9b3981687dfb74b
muhammadfaatihaflah-del <muhammadfaatihaflah@gmail.com>
2026-04-30 15:58:58 +0700

    deklarasi variabel di void addnode dan membuat fungsi if

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -22,6 +22,30 @@ public:
 
     void addnode()
     {
-        
+         int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+
+        Node *newnode = new Node;
+
+        newnode->noMhs = nim;
+
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if (START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate roll numbers not allowed" << endl;
+                return;
+            }
+
+            newnode->next = START;
+
+            if (START != NULL)
+                START->prev = newnode;
+
+            newnode->prev = NULL;
+            START = newnode;
+            return;
+        }
     }  
 };
